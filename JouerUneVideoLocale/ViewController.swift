28c1d4a179a8931d677a98a5ac3e42ae7774417f
Alain Boudreault <alain.boudreault@me.com>
2017-11-24 14:42:18 -0500

    version 1

--- JouerUneVideoLocale/ViewController.swift
+++ JouerUneVideoLocale/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var viewStreamDuNet: UIView!
+    
+    @IBOutlet weak var viewStreamLocal: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        chargerVideoÀPartirDuWeb()
+        chargerVideoLocale()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // ****************************************************
+    func chargerVideoÀPartirDuWeb() {
+        let url = URL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")
+        let player = AVPlayer(url: url!)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
 
+        // Ajuster la taille de la vidéo à celle de la view de présentation
+        playerViewController.view.frame = viewStreamDuNet.bounds
+        viewStreamDuNet.addSubview(playerViewController.view)
+
+        // Facultatif, Ajouter le panneau de control de la vidéo
+        self.addChildViewController(playerViewController)
+        player.play()
+    } // chargerVideoÀPartirDuWeb
+
+    // ****************************************************
+    func chargerVideoLocale() {
+        let url = Bundle.main.url(forResource: "piano", withExtension: "m4v")!
+        let player = AVPlayer(url: url)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        playerViewController.view.frame = viewStreamLocal.bounds
+        viewStreamLocal.addSubview(playerViewController.view)
+        
+        // Facultatif, Ajouter le panneau de control de la vidéo
+        self.addChildViewController(playerViewController)
+        player.play()
+    }
 }
 
