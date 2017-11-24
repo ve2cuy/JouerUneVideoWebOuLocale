60f3d2150ed2ce4e86782e00c1c0402024ecf060
Alain Boudreault <alain.boudreault@me.com>
2017-11-24 18:18:17 -0500

    Ajout de commentaires

--- JouerUneVideoLocale/ViewController.swift
+++ JouerUneVideoLocale/ViewController.swift
@@ -1,33 +1,42 @@
+//  Fichier:    ViewController.swift
+//  Projet:     JouerUneVideoLocale
+//  Auteur:     Alain Boudreault
+//  Copyright:  17-11-24 © ve2cuy, All rights reserved.
+//  ===================================================================================
+//  NOTE: À l'usage exclusif des étudiants et étudiantes de
+//  Techniques d'Intégration Multimédia
+//  du cégep de Saint-Jérôme.
+//  ----------------------------------------------------------------------------------
+//  Il est interdit de reproduire, en tout ou en partie, à des fins commerciales,
+//  le code source, les scènes, les éléments graphiques, les classes et
+//  tout autre contenu du présent projet sans l’autorisation écrite de l'auteur.
 //
-//  ViewController.swift
-//  JouerUneVideoLocale
+//  Pour obtenir l’autorisation de reproduire ou d’utiliser, en tout ou en partie,
+//  le présent projet, veuillez communiquer avec:
 //
-//  Created by Alain on 17-11-24.
-//  Copyright © 2017 Alain. All rights reserved.
+//  Alain Boudreault, aboudrea@cstj.qc.ca, ve2cuy.wordpress.com, ve2cuy @ github
 //
+//  ===================================================================================
 
+// Importation des librairies
 import UIKit
 import AVKit
 
+// ****************************************************
 class ViewController: UIViewController {
 
+    // Définition des liens MVC
     @IBOutlet weak var viewStreamDuNet: UIView!
-    
     @IBOutlet weak var viewStreamLocal: UIView!
     
-
+    // ****************************************************
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         chargerVideoÀPartirDuWeb()
         chargerVideoLocale()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    } // viewDidLoad()
+ 
     // ****************************************************
     func chargerVideoÀPartirDuWeb() {
         let url = URL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")
@@ -57,6 +66,7 @@ class ViewController: UIViewController {
         // Facultatif, Ajouter le panneau de control de la vidéo
         self.addChildViewController(playerViewController)
         player.play()
-    }
-}
+    } // chargerVideoLocale()
+
+} // ViewController
 
